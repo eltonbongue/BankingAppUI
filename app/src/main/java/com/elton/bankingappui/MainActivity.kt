aef41834519f9cafa5e431ba6ba08d2dbe808409
Elton <eltonbongue@gmail.com>
2025-06-27 10:55:12 +0100

    building HomeScreen

---
+++ app/src/main/java/com/elton/bankingappui/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.elton.bankingappui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.elton.bankingappui.ui.theme.BankingAppUITheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BankingAppUITheme {
+              HomeScreen()
+            }
+        }
+    }
+}
+
+
+
+@Preview
+@Composable
+fun HomeScreen(){
+    Scaffold (
+        bottomBar = {
+            BottomNavigationBar()
+
+        }
+            ){ padding ->
+
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ){
+
+            //  WalletSection()
+            //CardSection()
+            Spacer(modifier = Modifier.height(16.dp))
+            //FinanceSection()
+            //CurrenciesSection()
+
+        }
+
+
+
+    }
+
+}
+
+
+
+/*
+ @Composable
+ private fun SetBarColor(color: Color) {
+
+     val systemUiController = rememberSystemUiController()
+     SideEffect {
+         systemUiController.setSystemBarsColor(
+             color = color,
+
+         )
+     }
+
+
+ }
+
+   */
---
+++ app/src/main/java/com/elton/bankingappui/data/BottomNavigation.kt
@@ -0,0 +1,11 @@
+package com.elton.bankingappui.data
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavigation (
+
+    val title: String,
+    val icon: ImageVector
+
+
+)
